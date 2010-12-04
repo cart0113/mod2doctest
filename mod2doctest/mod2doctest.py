652642a656dff57dd8ff75092360aa9b6b1089f9
Andrew Carter <andrewjcarter@gmail.com>
2010-12-03 21:21:01 -0600

    Tweaked System Exit call

--- mod2doctest/mod2doctest.py
+++ mod2doctest/mod2doctest.py
@@ -37,7 +37,9 @@ import subprocess
 import re
 import doctest
 import time
-import pdb 
+import pdb
+import atexit
+
 
 DEFAULT_DOCTEST_FLAGS = (doctest.ELLIPSIS |
                          doctest.REPORT_ONLY_FIRST_FAILURE |
@@ -71,7 +73,7 @@ def convert(python_cmd,
     :type src:  True, module or file path
 
     :param target: Where you want the output docstring to be placed:
-       
+
                    * ``None``, the docstring is not saved anywhere (but it is
                      returned by this function and convert will not exit).
                    * ``True`` is given, the src module is used (the
@@ -79,11 +81,11 @@ def convert(python_cmd,
                    * A path (of type str) is provided, the docstr is saved to
                      that file.
                    * And finally, a simple convention: if a string is given
-                     that starts with '_' (e.g. '_doctest'), the output is saved 
+                     that starts with '_' (e.g. '_doctest'), the output is saved
                      to a file with the same name as the input, but with that
-                     string inserted right before the '.py' of the file name.  
-                     For example, if the src filename is 'mytest.py' and the 
-                     target is '_doctest' the docstring output will be saved to a 
+                     string inserted right before the '.py' of the file name.
+                     For example, if the src filename is 'mytest.py' and the
+                     target is '_doctest' the docstring output will be saved to a
                      file called 'mytest_doctest.py'
 
     :type target:  None, True, str file path, or str starting with '_'
@@ -97,14 +99,14 @@ def convert(python_cmd,
                         external file (str path).
     :type add_testmod:  True or False
 
-    :param ellipse_memid: Add ellipse for memory ids. 
+    :param ellipse_memid: Add ellipse for memory ids.
     :type ellipse_memid: True or False
 
-    :param ellipse_paths: Add ellipse for front path of path (up to final rel 
-                          path) 
+    :param ellipse_paths: Add ellipse for front path of path (up to final rel
+                          path)
     :type ellipse_paths:  True or False
 
-    :param ellipse_traceback: Ellipse middle part of traceback. 
+    :param ellipse_traceback: Ellipse middle part of traceback.
     :type ellipse_traceback: True or False
 
     :param run_doctest: If True doctest is run on the resulting docstring.
@@ -154,13 +156,12 @@ def convert(python_cmd,
         raise SystemError, ("'src' %s must be a valid module or file path, "
                             "or string ...") % obj
 
-    # First, remove the docstring.  Keep the input variable around, it's 
+    # First, remove the docstring.  Keep the input variable around, it's
     # needed later on (the raw input with just the docstring removed).
     input = _input_remove_docstring(input)
-    
-    #pdb.set_trace()
+
     pinput = _input_fix_whitespace(input)
-        
+
     pinput = _input_escape_shell_prompt(pinput)
     pinput = _input_remove_name_eq_main(pinput)
 
@@ -169,15 +170,16 @@ def convert(python_cmd,
     pinput = pinput.strip()
     pinput = pinput.replace('\r', '')
     pinput = pinput.replace('\t', '    ')
-    
-    shell = subprocess.Popen(args=[python_cmd, "-i"],
-                             shell=False,
+
+    popen = subprocess.Popen(args="%s -i" % python_cmd,
+                             bufsize=-1,
+                             shell=True,
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT,
                              )
 
-    docstr = _communicate(pinput, shell)
+    docstr = _communicate(pinput, popen)
 
     if ellipse_memid:
         docstr = _docstr_ellipse_mem_id(docstr)
@@ -203,20 +205,22 @@ def convert(python_cmd,
         doctitle = '%s\n' % _docstr_get_title()
     else:
         doctitle = '\n'
-        docstr = '\n'.join(docstr.split('\n')[2:]).lstrip()
+        docstr = '\n'.join(docstr.split('\n')[3:-2]).lstrip()
 
-    docstr = '"""%s%s\n\n"""' % (doctitle, docstr.strip())
-    
+    # Remember to remove any triple quotes """
+    docstr = docstr.replace("'''", '"""')
+
+    docstr = "'''%s%s\n\n'''" % (doctitle, docstr.strip())
+
     docstr = _docstr_cleanup_docstr(docstr)
 
     if target:
         target = _docstr_save(docstr, src, target, input, add_testmod)
         if run_doctest:
             _run_doctest(target, doctest_flags)
-        raise SystemExit    
+        raise SystemExit
     else:
         return docstr
-
 
 _ADD_TESTMOD_STR = """
 if __name__ == '__main__':
@@ -224,14 +228,14 @@ if __name__ == '__main__':
     doctest.testmod(optionflags=%d)
 """
 
-_RE_INPUT_PROCESS = re.compile(r'''
+_RE_INPUT_PROCESS = re.compile(r"""
 ^\s*            # Start of file, exclude whitespace
 (r|R)?          # Allow for raw-strings
-"""             # First triple quote.
+'''             # First triple quote.
 (.|\n)*?        # Everything in-between, but a non-greedy match
-"""             # Second triple quote.
+'''             # Second triple quote.
 \s*             # Get rid of all other whitespace.
-''', flags=re.VERBOSE)
+""", flags=re.VERBOSE)
 def _input_remove_docstring(input):
     """Strips the input and removes the first docstring from the input.
 
@@ -252,37 +256,37 @@ def _input_escape_shell_prompt(input):
 
 def _input_fix_whitespace(input):
     """Fixes problems with spaces in the input that cause interpreter errors.
-    
+
     There are two major problems and will use ^ to denote spaces
     Normally, a statement like::
 
         def fn():
-        
+
         ^^^^print 'foobar'
-        
+
     will not work if directly copied / pasted because there are no spaces after
     the ``def fn():`` line.
-    
+
     Also::
-    
+
         def fn():
         ^^^^print 'hi'
         def fx():
         ^^^^print 'bye'
-    
+
     does not allow direct copy paste either (you need a newline betwen the fn
-    calls). This will run fine within a module but **not** if pasted into the 
+    calls). This will run fine within a module but **not** if pasted into the
     interpreter. This function fixes these problem.
-    
+
     The strategy is to collect all whitespace/comments and hold them in the
-    ``hold`` list.  Then, track the current indent level.  When a non 
-    whitespace/comment line is encountered, apply the stored whitespace 
+    ``hold`` list.  Then, track the current indent level.  When a non
+    whitespace/comment line is encountered, apply the stored whitespace
     with the correct level of indentation.  Also, add a blank line between
-    statements like those shown above. 
+    statements like those shown above.
 
     .. note::
-    
-       This function makes assumptions about the Python syntax as taken 
+
+       This function makes assumptions about the Python syntax as taken
        from http://docs.python.org/reference/compound_stmts.html
 
     """
@@ -294,11 +298,11 @@ def _input_fix_whitespace(input):
     stackable_tokens = set(['else', 'elif', 'except', 'finally'])
     for line in input.split('\n'):
         left_stripped = line.lstrip()
-        if not left_stripped or left_stripped.startswith('#'): 
+        if not left_stripped or left_stripped.startswith('#'):
             # collect all blank lines or full comment lines
             hold.append(left_stripped)
         else:
-            new_indent = len(line) - len(left_stripped)    
+            new_indent = len(line) - len(left_stripped)
             if left_stripped.split()[0].split(':')[0] not in stackable_tokens:
                 # Add whitespace if, for example, two functions are defined
                 # with no whitespace in between
@@ -313,8 +317,8 @@ def _input_fix_whitespace(input):
             lines.append(line.rstrip())
 
     return '\n'.join(lines)
-    
-_RE_NEM = re.compile(r"""^if\s+__name__\s*==\s*['"]+__main__['"]+\s*:""", 
+
+_RE_NEM = re.compile(r"""^if\s+__name__\s*==\s*['"]+__main__['"]+\s*:""",
                      flags=re.IGNORECASE)
 def _input_remove_name_eq_main(input):
     lines = []
@@ -329,59 +333,55 @@ def _input_remove_name_eq_main(input):
             lines.append(line)
     return '\n'.join(lines)
 
-def _communicate(pinput, shell):
-    
-    shell.stdin.write('%s\n\nraise SystemExit\n\n' % pinput)
-    shell.stdin.flush()
-    
+def _communicate(pinput, popen):
+
+    pinput = '%s\n\nraise SystemExit\n\n' % pinput
+
     pinputlines = pinput.split('\n')
+
+    stdout, stderr = popen.communicate(pinput)
+
     docstrlines = []
 
     intraceback = False
     startheader = True
 
-    while True:
-    
-        outputline = shell.stdout.readline()
+    for outputline in stdout.split('\n'):
+        outputline = outputline.replace('\r', '')
+        outputline = outputline.replace('\t', '    ')
 
-        if not outputline:
-            break
-        else:
-            outputline = outputline.replace('\r', '')
-            outputline = outputline[:-1]
-            outputline = outputline.replace('\t', '    ')
-        
-            for line in _match_input_to_output(pinputlines, outputline):
+        for line in _match_input_to_output(pinputlines, outputline):
 
-                docstrlines.append(line)
-            
-                if line.startswith('>>> #>') or line.startswith('... #>'):
-                    if startheader:
-                        print '\n'
-                    line = line[6:]
-                    startheader = False
-                elif line.startswith('>>> ') or line.startswith('...'):
-                    startheader = True
-                    continue
-                
-                if line.startswith('Traceback') or line.startswith('Exception'):
-                    intraceback = True
-                elif intraceback and not line.startswith(' '):
-                    intraceback = None
-                
-                if intraceback is False:
-                    print line
-                else:
-                    print >> sys.stderr, line
-                    time.sleep(0.010)
-                    
-                if intraceback is None:
-                    intraceback = False
-                    
+            docstrlines.append(line)
+
+            if line.startswith('>>> #>') or line.startswith('... #>'):
+                if startheader:
+                    print '\n'
+                line = line[6:]
+                startheader = False
+            elif line.startswith('>>> ') or line.startswith('...'):
+                startheader = True
+                continue
+
+            if line.startswith('Traceback') or line.startswith('Exception'):
+                intraceback = True
+            elif intraceback and not line.startswith(' '):
+                intraceback = None
+
+            if intraceback is False:
+                print line
+            else:
+                time.sleep(0.010)
+                print >> sys.stderr, line
+                time.sleep(0.010)
+
+            if intraceback is None:
+                intraceback = False
+
     return '\n'.join(docstrlines)
-    
+
 def _match_input_to_output(inputlines, outputline):
-    
+
     has_input = True
 
     while has_input:
@@ -455,18 +455,15 @@ def _docstr_ellipse_traceback(docstr):
     return _RE_ELLIPSE_TRACEBACK.sub(r'\1\n    ...\2', docstr)
 
 _RE_LOCAL_PATH = re.compile(r"""
-                             (\s+['|\"]+)
-                             (?:/+|[a-zA-Z]:\\+)
-                             .*
-                             (?:/+|\\+)
-                             ((?:\w|\.)+['|\"]+)
+                             (?:\S*(?:[/\\][^\s/\\]+)+)
+a                            ([/\\][^\s/\\]+)
                              """, flags=re.VERBOSE)
 def _docstr_ellipse_paths(output):
-    return _RE_LOCAL_PATH.sub(r'\1...\2', output)
+    return _RE_LOCAL_PATH.sub(r'...\1', output)
 
 _RE_PRINT_MARKER = re.compile(r'(?:>>>|...)\s#[>|]')
 def _process_docstr_markers(docstr):
-    
+
     lines = []
     in_print = False
 
@@ -477,7 +474,7 @@ def _process_docstr_markers(docstr):
             in_print = True
             if len(line) >= 8 and line[6] == ' ' and line[7] != '':
                 line = line[7:]
-            else:    
+            else:
                 line = line[6:]
         elif line.startswith('>>> ') or line.startswith('... '):
             if in_print is True and line.strip() == '...':
@@ -487,9 +484,9 @@ def _process_docstr_markers(docstr):
                 lines.append(' ')
                 if line.startswith('...'):
                     line = '>>> %s' % line[4:]
-        
+
         lines.append(line)
-            
+
     return '\n'.join(lines)
 
 def _docstr_cleanup_docstr(docstr):
@@ -506,7 +503,7 @@ def _docstr_cleanup_docstr(docstr):
                 lines.append(extra_marker)
             extra_marker = None
             lines.append(line)
-            
+
     return '\n'.join(lines)
 
 def _run_doctest(target, doctest_flags):
